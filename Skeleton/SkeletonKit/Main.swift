abb52b752b5c56583f31674e25b19eccc17196e4
dcilia <dcilia@dcilias-MacBook-Pro.local>
2018-08-22 14:50:33 -0400

    Initial Commit

---
+++ Skeleton/SkeletonKit/Main.swift
@@ -0,0 +1,16 @@
+//
+//  Main.swift
+//  SkeletonKit
+//
+//  Created by  dcilia on 8/22/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import Foundation
+
+public struct Foo {
+    public var name : String = "Steve"
+    public init() {
+
+    }
+}
---
+++ Skeleton/SkeletonKit/SkeletonKit.h
@@ -0,0 +1,19 @@
+//
+//  SkeletonKit.h
+//  SkeletonKit
+//
+//  Created by  dcilia on 8/22/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SkeletonKit.
+FOUNDATION_EXPORT double SkeletonKitVersionNumber;
+
+//! Project version string for SkeletonKit.
+FOUNDATION_EXPORT const unsigned char SkeletonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkeletonKit/PublicHeader.h>
+
+
